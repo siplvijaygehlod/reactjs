d6cbfdebe08d259f97e52c49e7d0ac3c5f1892c5
siplvijaygehlod <vijay.gehlod@systematixindia.com>
2019-02-09 18:54:44 +0530

    src delete and added new files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function transform(offset) {
+  const cos = Math.cos(offset);
+  const sin = Math.sin(offset);
+  return {
+    transform: `matrix3d(${sin}, ${-cos}, ${sin}, 0, ${-cos}, ${sin}, 0, 0, 0, ${cos}, ${cos}, ${sin}, 0, 0, 0, 1)`
+  };
+}
+
+class App extends React.Component {
+  state = { styleOne: {}, styleTwo: {} };
+
+  onMouseMove = event => {
+    this.setState({
+      styleOne: transform(-event.ClientX / event.ClientY),
+      styleTwo: transform(event.ClientX / event.ClientY)
+    });
+  };
+
+  render() {
+    return (
+      <div onMouseMove={this.onMouseMove}>
+        <div className="panel" style={this.state.styleone} />
+        <div className="panel" style={this.state.styleTwo} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
