3e376a8ebf64eacf5940dd5f15d60fc816910d81
siplvijaygehlod <vijay.gehlod@systematixindia.com>
2019-02-11 14:45:25 +0530

    basic intro

--- src/index.js
+++ src/index.js
@@ -1,33 +1,108 @@
-import React from "react";
-import ReactDOM from "react-dom";
+//import React from 'react';
+//import ReactDOM from 'react-dom';
 
 
-function transform(offset) {
-  const cos = Math.cos(offset);
-  const sin = Math.sin(offset);
-  return {
-    transform: `matrix3d(${sin}, ${-cos}, ${sin}, 0, ${-cos}, ${sin}, 0, 0, 0, ${cos}, ${cos}, ${sin}, 0, 0, 0, 1)`
-  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** 
+function getButtonData() {
+  return 'clicke here 1';
 }
+const App = () => {
+  //const buttonText = 'Click here';
+  return ( <div >
+    <label className="label" htmlFor="name"> Name: </label> <input id = "name"
+    type = "text" / >
+    <button style = {
+      {
+        backgroundColor: 'red',
+        color: 'white'
+      }
+    } > {
+      getButtonData()
+    } </button> </div>
+  );
+};
 
-class App extends React.Component {
-  state = { styleOne: {}, styleTwo: {} };
+ReactDOM.render( <App /> ,
+  document.querySelector('#root')
+);
 
-  onMouseMove = event => {
-    this.setState({
-      styleOne: transform(-event.ClientX / event.ClientY),
-      styleTwo: transform(event.ClientX / event.ClientY)
-    });
-  };
 
-  render() {
-    return (
-      <div onMouseMove={this.onMouseMove}>
-        <div className="panel" style={this.state.styleone} />
-        <div className="panel" style={this.state.styleTwo} />
-      </div>
-    );
-  }
-}
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+
+//function transform(offset) {
+//   const cos = Math.cos(offset);
+//   const sin = Math.sin(offset);
+//   return {transform: `matrix3d(${sin}, ${-cos}, ${sin}, 0, ${-cos}, ${sin}, 0, 0, 0, ${cos}, ${cos}, ${sin}, 0, 0, 0, 1)`};
+// }
+
+// class App extends React.Component {
+//   state = {styleOne: {}, styleTwo: {}};
+
+//   onMouseMove = (event) => {
+//       this.setState({
+//           styleOne: transform(-event.clientX / event.clientY),
+//           styleTwo: transform(event.clientX / event.clientY)
+//       });
+//   }
+
+
+//   render() {
+//       return ( 
+//           <div onMouseMove={this.onMouseMove}>
+//               <div className="panel" style={this.state.styleOne}/>
+//               <div className="panel" style={this.state.styleTwo}/>
+//           </div>
+//       )
+//   }
+// }
+
+// ReactDOM.render(
+//   <App/>,
+//   document.querySelector('#root')
+// );
+
+*/
